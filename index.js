356379db89799d523e1ec944174eceae848d4dae
X <69140648+Xs-Return@users.noreply.github.com>
2024-02-22 08:27:18 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, EmbedBuilder, Embed } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder, Events, Partials } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers,
-    ]
+        GatewayIntentBits.GuildMessageReactions,
+    ],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
 const fs = require('fs');
-
+const SharedData = {
+    nameArray: [],
+}
 // Function to read server channels data from JSON file
 function readServerChannels() {
     try {
@@ -35,20 +39,25 @@ function writeServerChannels(serverChannels) {
 
 // Function to fetch data from the JSON file
 function fetchData() {
-  fetch('https://cdn2.arkdedicated.com/servers/asa/officialserverlist.json')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-      
-    })
-    .then(data => {
-      // Call the processData function with the fetched data
-      processData(data);
-    })
-    .catch(error => {
-      console.error('There was a problem fetching the data:', error);
+
+
+    return new Promise((resolve, reject) => {
+        fetch('https://cdn2.arkdedicated.com/servers/asa/officialserverlist.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Resolve the promise with the fetched data
+                resolve(data);
+                processData(data);
+            })
+            .catch(error => {
+                // Reject the promise if there was an error fetching the data
+                reject(error);
+            });
     });
 }
 
@@ -59,7 +68,7 @@ const previousNumPlayers = new Map();
 async function processData(data) {
   let serverChannels = readServerChannels();
     const desiredNames = serverChannels.map(pair => pair.serverName); // Extract server names from serverChannels array
-    
+    this.nameArray = data.map(obj => obj.Name);
     // Filter entries based on server names from serverChannels
     const filteredEntries = data.filter(entry => desiredNames.includes(entry.Name));
 
@@ -74,7 +83,12 @@ async function processData(data) {
         // Fetch the channel
         const channel = client.channels.cache.get(channelId);
         if (!channel) {
-            console.error(`Channel not found: ${channelId}`);
+            const idx = serverChannels.findIndex(element => element.channelId === channelId)
+            serverChannels.splice(idx,1)
+            console.log(serverChannels)
+            console.log(`Channel not found: ${channelId} Removing from Array.`);
+            writeServerChannels(serverChannels);
+
             continue;
         }
 
@@ -101,13 +115,14 @@ async function processData(data) {
                 embed.setTimestamp()
                 embed.setAuthor({ name: 'Sheogorath', iconURL: 'https://imgur.com/RlyINGK.png'})
                 embed.setFooter({ text: `v${entry.BuildId}.${entry.MinorBuildId}`,})
-            });
 
+            });
+            
             // Edit the message with the updated embed
             try {
                 const messages = await channel.messages.fetch({ limit: 1 });
                 const lastMessage = messages.first();
-                if (lastMessage) {
+                if (lastMessage && lastMessage.author.id === client.user.id) {
                     await lastMessage.edit({ embeds: [embed] });
                     console.log(`Embed updated successfully in ${channel.name} (${channelId})`);
                 } else {
@@ -117,8 +132,6 @@ async function processData(data) {
             } catch (error) {
                 console.error(`Error updating/embedding message in channel ${channelId}: ${error}`);
             }
-        } else {
-            console.log(`NumPlayers value has not changed for ${serverName}, skipping update`);
         }
     }
 }
@@ -131,14 +144,181 @@ async function processData(data) {
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'status') {
+  if (interaction.commandName === 'watch') {
     let serverChannels = readServerChannels();
-      const serverName = interaction.options.getString('name');
-      const channelId = interaction.channelId;
-      serverChannels.push({ serverName, channelId });
-      writeServerChannels(serverChannels);
-      await interaction.reply(`Server "${serverName}" added to the list. in channel "${channelId}"`);
+    const partialserverName = interaction.options.getString('name');
+    const channelId = interaction.channelId;
+    const serverName = nameArray.find(name => name.includes(partialserverName));
+    if (!serverName){
+        try {
+            await interaction.reply({ content: 'Cant Finder Server Info', ephemeral: true });
+            return console.log("badName")
+        }
+        catch {return console.log("NoPerms")}
+      
+    } 
+    serverChannels.push({ serverName, channelId });
+    writeServerChannels(serverChannels);
+    try {
+    await interaction.reply({ content: `Server ${serverName} added to the list.`, ephemeral: true });
+}
+catch {return console.log("NoPerms")}
+  } 
+  else if (interaction.commandName === 'server') {
+    const partialserverName = interaction.options.getString('name');
+    const serverName = nameArray.find(name => name.includes(partialserverName));
+    if (!serverName){
+        try {
+            await interaction.reply({ content: 'Cant Finder Server Info', ephemeral: true });
+            return console.log("badName")
+        }
+        catch {return console.log("NoPerms")}
+    }
+    fetchData()
+    .then(data => {
+        const filteredEntries = data.filter(entry => serverName.includes(entry.Name));
+const embed = new EmbedBuilder()
+                .setTitle(`${serverName}`);
+
+            // Add fields for each filtered entry
+                const numPlayers = filteredEntries[0].NumPlayers;
+                const maxPlayers = filteredEntries[0].MaxPlayers;
+                embed.addFields({ name: "Players:", value: "```"+`${numPlayers}/${maxPlayers}`+"```",inline: true},{ name: "Map:", value: "```"+`${filteredEntries[0].MapName}`+"```",inline: true},{ name: "Day:", value: "```"+`${filteredEntries[0].DayTime}`+"```",inline: true},{ name: "IP:", value: "```"+`${filteredEntries[0].IP}:${filteredEntries[0].Port}`+"```",inline: true});
+                embed.setTimestamp()
+                embed.setAuthor({ name: 'Sheogorath', iconURL: 'https://imgur.com/RlyINGK.png'})
+                embed.setFooter({ text: `v${filteredEntries[0].BuildId}.${filteredEntries[0].MinorBuildId}`,})
+                try {
+                    interaction.reply({ embeds: [embed] });
+                }
+                catch {console.log("NoPerms")}
+
+    })
+    .catch(error => {
+        console.error('There was a problem fetching the data:', error);
+    });
+
   }
+  else if (interaction.commandName === 'watcherclear') {
+    let serverChannels = readServerChannels();
+      const channelId = interaction.channelId;
+      for (let i = serverChannels.length - 1; i >= 0; i--) {
+        if (serverChannels[i].channelId === channelId) {
+            serverChannels.splice(i, 1); // Remove the item at index i
+        }
+    }
+      console.log(serverChannels)
+      console.log(`Channel not found: ${channelId} Removing from Array.`);
+      writeServerChannels(serverChannels);
+
+      try {
+        await interaction.reply({ content: 'Cleared All Listeners', ephemeral: true });
+    }
+    catch {console.log("NoPerms")}
+  } else if (interaction.commandName === 'checklist') {
+    await interaction.deferReply({ ephemeral: true });
+    const items = interaction.options.getString('items');
+    const itemList = items.split(',');
+    try {
+    for (const item of itemList) {
+        const message = await interaction.channel.send(`🔳 ${item}`);
+        await message.react('✅');  // Add a white checkmark reaction
+    }
+
+    await interaction.editReply({ content: 'Checklist created!', ephemeral: true });
+}
+    catch {
+        console.log("NoPerms")
+    }
+} else if (interaction.commandName === 'clearlist') {
+    await interaction.deferReply({ ephemeral: true });
+    const fetched = await interaction.channel.messages.fetch({ limit: 100 });
+    const botMessages = fetched.filter(msg => msg.author.bot);
+
+    for (const msg of botMessages.values()) {
+        await msg.delete();
+    }
+    try {
+        await interaction.editReply({ content: 'All bot messages cleared!', ephemeral: true });
+    }
+        catch {
+            console.log("NoPerms")
+        }
+    
+} else if (interaction.commandName === 'purgechannel') {
+    await interaction.deferReply({ ephemeral: true });
+    const channelId = interaction.options.getString('channelid')
+    const channel = client.channels.cache.get(channelId)
+    const fetched = await channel.messages.fetch({ limit: 100 });
+    const botMessages = fetched.filter(msg => msg.author.bot);
+
+    for (const msg of botMessages.values()) {
+        await msg.delete();
+    }
+    try {
+        await interaction.editReply({ content: 'All bot messages cleared!', ephemeral: true });
+    }
+        catch {
+            console.log("NoPerms")
+        }
+    
+}
+});
+
+//Callback
+function embedCallback(data,ServerName) {
+    let serverChannels = readServerChannels();
+    const partialserverName = interaction.options.getString('name');
+    const channelId = interaction.channelId;
+    const serverName = nameArray.find(name => name.includes(partialserverName));
+    if (!serverName){
+
+        try {
+            interaction.reply({ content: 'Cant Finder Server Info', ephemeral: true });
+            return console.log("badName")
+        }
+        catch {console.log("NoPerms")}
+      
+    } 
+    console.log(serverName);
+    serverChannels.push({ serverName, channelId });
+    writeServerChannels(serverChannels);
+
+    try {
+        interaction.reply({ content: `Server ${serverName} added to the list.`, ephemeral: true });
+        return console.log("badName")
+    }
+    catch {console.log("NoPerms")}
+
+    
+}
+
+
+//Reactions
+client.on(Events.MessageReactionAdd, async (reaction, user) => {
+    try {
+console.log("reactionhi")
+        if (reaction.partial) {
+            // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+            try {
+                await reaction.fetch();
+            } catch (error) {
+                console.error('Something went wrong when fetching the message:', error);
+                // Return as `reaction.message.author` may be undefined/null
+                return;
+            }
+        }
+
+        if (user.bot) return; // Ignore reactions from bots
+
+        if (reaction.emoji.name === '✅') {
+            const message = reaction.message;
+            if (!message.content.startsWith('🔳')) return; // ignore messages that are not checklist items
+            await message.edit({ content: `✅ ${message.content.slice(2)} (completed by <@${user.id}>)` }); // mark the item as completed and mention the user
+            await message.reactions.removeAll()
+        }
+    } catch (error) {
+        console.error(`An error occurred in messageReactionAdd: ${error}`);
+    }
 });
 
 // When the bot is ready, start fetching data
--- register-commands.js
+++ register-commands.js
@@ -3,17 +3,17 @@ const { REST , Routes, ApplicationCommandOptionType} = require('discord.js');
 
 const commands = [
     {
-      name: 'status',
-      description: 'Add Server Listener to Channel',
-      options: [
-        {
-          name: 'name',
-          description: 'The name of the server',
-          type: ApplicationCommandOptionType.String,
-          required: true
-        }
-      ]
-    }
+        name: 'purgechannel',
+        description: 'clears a channel of a given id',
+        options: [
+          {
+            name: 'channelid',
+            description: 'The name of the server',
+            type: ApplicationCommandOptionType.String,
+            required: true
+          },
+        ]
+      }
 ];
 
 const rest = new REST({version: "10"}).setToken(process.env.TOKEN);
@@ -25,8 +25,7 @@ const rest = new REST({version: "10"}).setToken(process.env.TOKEN);
 
     await rest.put(
 Routes.applicationGuildCommands(
-    process.env.CLIENT_ID,
-    process.env.GUILD_ID
+    process.env.CLIENT_ID,process.env.GUILD_ID
     ),
     { body: commands }
     );
